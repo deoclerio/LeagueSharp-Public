5b47464476c27b25304ac69a77275b97b25a946e
fueledbyflux <fueledbyflux@gmail.com>
2014-09-19 16:38:22 +1000

    removed igniter dependancy

--- SigmaFizz/SigmaFizz/Program.cs
+++ SigmaFizz/SigmaFizz/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Igniter;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
